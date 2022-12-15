a76da8f719b3bd48c11eaaf6aa0c6f23ce3e1ccd
Harshil Jain <64075179+Panther0473@users.noreply.github.com>
2022-12-15 23:43:42 +0530

    Add files via upload

---
+++ Min_max_Sum.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+void min_max(long int* arr)
+{
+    long int max= 0,min = 0;
+    for(int i = 0; i < 4; i ++)
+    {
+        min += *(arr + i);  
+    }
+    
+    for(int i = 4; i > 0 ; i --)
+    {
+        max += *(arr + i);
+    }
+    
+    cout<<min<<" "<<max;
+}
+
+void insertion_sort(long int* arr)
+{
+    long int temp,j;
+    for(int i = 1 ; i < 5 ; i ++)
+    {
+        temp = *(arr + i);
+        j = i -1;
+        
+        while((j >= 0) && ((*(arr + j)) > temp))
+        {
+            *(arr + j + 1) = *(arr + j);
+            j = j - 1;
+        }
+        *(arr + j + 1) = temp;
+    }
+}
+
+int main()
+{
+    long int arr[5];
+    
+    for(int i = 0; i < 5 ; i ++)
+    {
+        cin>>*(arr + i);
+    }
+    
+    insertion_sort(arr);
+    min_max(arr);
+    
+    return 0;
+}
